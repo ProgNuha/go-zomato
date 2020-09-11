ca713acab2e69a7bba376be4da0a73e983df278a
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-09-11 11:32:57 +0700

    feat(header): add header component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
-function App() {
+const Post = ({ src }) => <img src={src} alt="profile" />;
+
+const Header = (props) => {
+  const {username} = props;
+  const [city, setCity] = useState("Jakarta");
+  const postCount =5;
+  const posts = [...Array(postCount)];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>GoZomato</div>
+      <div>@{username}</div>
+      <input onChange={({target: {value} }) => {
+        setCity(value);
+      }}
+      
+        placeholder={`Current city: ${city}`}
+      />
+
+      <button onClick={() => setCity()}>Change city</button>
+      
+      {postCount === 0 ? (
+        <div>No Posts</div>
+      ) : (
+        posts.map((_, idx) => (
+          <div class="card">
+            <div>
+              <Post src="https://via.placeholder.com/150" key={idx}/>
+            </div>
+            <div>name</div>
+            <div>cuisine</div>
+            <div>budget</div>
+            <div>rating</div>
+          </div>
+        ))
+        )
+        
+      }
+      
+    </>
   );
 }
 
-export default App;
+
+export default function App() {
+
+
+  const username = "mufidanuha";
+  return <Header username={username}/>
+}
+
